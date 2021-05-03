d812867c0784e7b9be3e61adf9506eb1890ff314
ledevpole <ledevpole@gmail.com>
2021-05-03 14:31:25 +0200

    sending amazing larache code

---
+++ README.md
@@ -0,0 +1,1 @@
+# todo_methode_larache
---
+++ js/main.js
@@ -0,0 +1,19 @@
+window.onload = function(){
+
+    const list = document.querySelector('#list-todo')  
+
+    function addElement(){
+        element = document.createElement('li')
+        let value = document.getElementById('text-entry').value
+        
+        element.id = list.childElementCount
+        element.innerHTML = `${value} <input  type="button" class="del-btn" name="${list.childElementCount}" value="delete"/>`
+
+        element.querySelector(':first-child').addEventListener("click", (event) => {
+            document.getElementById(event.target.name).remove()
+        })
+        list.appendChild(element)
+    }
+
+    document.getElementById('add-item').addEventListener("click", addElement)
+}
