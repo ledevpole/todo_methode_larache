046c3d89ec84f4d74dfff5c3d24a108b9fca81b0
ledevpole <ledevpole@gmail.com>
2021-05-03 15:32:21 +0200

    adding timing tasks

--- js/main.js
+++ js/main.js
@@ -12,7 +12,27 @@ window.onload = function(){
         element.querySelector(':first-child').addEventListener("click", (event) => {
             document.getElementById(event.target.name).remove()
         })
+
         list.appendChild(element)
+
+        setTimeout(alert,10000,element.id)
+    }
+
+    function alert(id){
+        console.log("ici")
+        document.getElementById(id).classList.add("late");
+        setTimeout(alert2,5000,id)
+    }
+
+    function alert2(id){
+        console.log("alert2")
+        document.getElementById(id).classList.add("green");
+        setTimeout(alert3,5000,id)
+    }
+
+    function alert3(id){
+        console.log("alert3")
+        document.getElementById(id).classList.add("blue");
     }
 
     document.getElementById('add-item').addEventListener("click", addElement)
